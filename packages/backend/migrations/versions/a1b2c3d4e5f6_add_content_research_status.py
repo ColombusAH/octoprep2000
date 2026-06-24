8d8e05fa17abb2a28f226439b175e5ff8e341614
deangev <gevadean@gmail.com>
2026-06-24 12:43:23 +0300

    Update migration script to reflect new down_revision ID for content_research_status addition

--- packages/backend/migrations/versions/a1b2c3d4e5f6_add_content_research_status.py
+++ packages/backend/migrations/versions/a1b2c3d4e5f6_add_content_research_status.py
@@ -1,7 +1,7 @@
 """add content_research_status to reports
 
 Revision ID: a1b2c3d4e5f6
-Revises: 9bb93c193c03
+Revises: d7e1f4a902bc
 Create Date: 2026-06-24
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "a1b2c3d4e5f6"
-down_revision: Union[str, None] = "9bb93c193c03"
+down_revision: Union[str, None] = "d7e1f4a902bc"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
